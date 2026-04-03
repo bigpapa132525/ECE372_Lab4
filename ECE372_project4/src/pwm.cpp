3428935c746c28a5120e62b34570f3ff220d9dbf
Dylan <dylancorrea13@arizona.edu>
2026-04-02 17:46:52 -0700

    Updated PWM
    The PWM file can change, read, and initialize everything now. Still needs to update rotation

--- ECE372_project4/src/pwm.cpp
+++ ECE372_project4/src/pwm.cpp
@@ -11,15 +11,15 @@
 // Use Fast PWM Mode #14 if you need more flexibilty on the frequency value.
 
 // I will have a prescaler of 1.  The calculation of OCR1A are shown below:
-void initPWMTimer1()  {
+void initPWMTimer3(){
 
  //set header pin 11 to output
-DDRB |= (1 << DDB5);
+DDRE |= (1 << DDE3);
   // set non-inverting mode - output starts high and then is low, 
   //COM1A0 bit = 0
   //COM1A1 bit =1
-  TCCR1A |= (1 << COM1A1);
-  TCCR1A &= ~(1 << COM1A0);
+  TCCR3A |= (1 << COM3A1);
+  TCCR3A &= ~(1 << COM3A0);
 
   //  Use fast PWM mode 10 bit, top value is determined by Table 17-2 of 0x3FF (1023) 
   //  which determines the PWM frequency.
@@ -28,10 +28,10 @@ DDRB |= (1 << DDB5);
 // WGM11 =1
 // WGM12 = 1
 // WGM13 = 0
-TCCR1A |=  (1 << WGM10) | (1 << WGM11);
+TCCR3A |=  (1 << WGM30) | (1 << WGM31);
 
-TCCR1B |= (1 << WGM12);
-TCCR1B &= ~(1 << WGM13); 
+TCCR3B |= (1 << WGM32);
+TCCR3B &= ~(1 << WGM33); 
 
 // PWM frequency calculation for FAST PWM mode on page 148 of datasheet
   //frequency of PWM = (F_clk)/((Prescaler)* (1 +TOP))
@@ -42,12 +42,14 @@ TCCR1B &= ~(1 << WGM13);
 
 
 
-// set prescalar CSBits to prescaler of 1
-//CS10 =1
-//CS11 =0
+// set prescalar CSBits to prescaler of 8
+    //gives frequency of 1953.125 Hz
+    //The motor is rated for 5k so this is good
+//CS10 =0
+//CS11 =1
 //CS12 =0
-TCCR1B |= (1 << CS10);
-TCCR1B &= ~((1 << CS11)  | (1 << CS12));
+TCCR3B |= (1 << CS11);
+TCCR3B &= ~((1 << CS10)  | (1 << CS12));
 
 
 // the last thing is to set the duty cycle.     
@@ -56,6 +58,27 @@ TCCR1B &= ~((1 << CS11)  | (1 << CS12));
 //  calculate OCR1A value => OCR1A = duty cycle(fractional number) * (1 + TOP) 
 // we want a duty cycle = 60%
 // OCR1A = 0.60 * 1024
- OCR1A =  255;
+ OCR3A =  255;
+}
+
+int readADC(){
+  // read in ADCL first then ADCH
+    int result = ADCL;
+    result += ((unsigned int) ADCH) << 8;
+    // int voltage = result * (4.586/1024.0);
+  return result;
+}
+
+/* ------------------ Change Duty Cycle with ADC -------------------- */
+void changeDutyCycle(int adcValue){
+    if (adcValue < 462)
+    {
+      OCR3A = 2 * (426 - adcValue);
+    }
+    else if(adcValue > 562 ){
+      OCR3A = 2 * (adcValue - 562);
+    }
+    else
+      OCR3A = 0;
 }
 
--- ECE372_project4/src/pwm.h
+++ ECE372_project4/src/pwm.h
@@ -5,6 +5,8 @@
 #ifndef PWM_H
 #define PWM_H
 
-void initPWMTimer1();
+void initPWMTimer3();
+void changeDutyCycle(int adcValue);
+int readADC();
 
 #endif
